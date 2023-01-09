a2f3248b433f6fabfb4bf2e42b409e7aa15db397
Sandeep Subedi <sandeep1111able@gmail.com>
2023-01-09 22:30:12 +0545

    Create button to enter grid number and refactor functions

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 const container = document.querySelector(".container");
-
-for (let i = 0; i < 16; i++) {
-  const div = document.createElement("div");
-  div.classList.add("grid-box");
-  container.append(div);
-}
+const gridBtn = document.querySelector("button");
+const containerWidth = container.offsetWidth - 6;
+let gridNumber = 4;
 
 const randomRGB = () => {
   const colorArray = [
@@ -32,10 +29,36 @@ const randomRGB = () => {
   return hexCode;
 };
 
-const divs = document.querySelectorAll(".grid-box");
+const createDiv = (number) => {
+  for (let i = 0; i < number ** 2; i++) {
+    const div = document.createElement("div");
+    div.classList.add("grid-box");
+    div.style.width = `${containerWidth / gridNumber}px`;
+    div.style.height = `${containerWidth / gridNumber}px`;
+    container.append(div);
+  }
+};
+const getDivColor = () => {
+  const divs = document.querySelectorAll(".grid-box");
 
-divs.forEach((div) => {
-  div.addEventListener("mouseover", () => {
-    div.style.backgroundColor = randomRGB();
+  divs.forEach((div) => {
+    div.addEventListener("mouseover", () => {
+      div.style.backgroundColor = randomRGB();
+    });
   });
+};
+
+createDiv(gridNumber);
+getDivColor();
+
+gridBtn.addEventListener("click", () => {
+  let number = +prompt("Enter your number");
+  if (typeof number !== "number" || number >= 100 || number <= 4) {
+    alert("Please enter a valid number between 4 and 100");
+  } else {
+    container.innerHTML = "";
+    gridNumber = number;
+  }
+  createDiv(gridNumber);
+  getDivColor();
 });
